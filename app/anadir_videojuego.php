b49f7b144fd22aded596dede2a1ec4bd7b5a5db6
Ander03 <agorocica002@ikasle.ehu.eus>
2023-09-30 17:01:26 +0200

    Prueba

---
+++ app/anadir_videojuego.php
@@ -0,0 +1,50 @@
+<?php
+include("config.php"); // Incluye el archivo de configuración
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Recupera los datos del formulario
+    $nombre = $_POST["nombre"];
+    $creador = $_POST["creador"];
+    $productora = $_POST["productora"];
+    $genero = $_POST["genero"];
+    $sistema_operativo = $_POST["sistema_operativo"];
+    $fecha_lanzamiento = $_POST["fecha_lanzamiento"];
+ 
+
+
+    //Validar parametros (Falta hacer)
+
+
+    
+    // Verifica la conexión
+    if (!$conn) {
+        die("La conexión a la base de datos falló: " . mysqli_connect_error());
+    }
+
+    // Crea la consulta SQL para insertar el nuevo usuario en la tabla
+    $sql = "INSERT INTO mytable (Name, Developer, Producer, Genre, Operating_System, Date_Released) VALUES (?, ?, ?, ?, ?, ?)";
+
+    // Prepara la consulta SQL
+    $stmt = mysqli_prepare($conn, $sql);
+
+    //Verificar que la función 'mysqli_prepare' haya tenido éxito
+    if (!$stmt) {
+        die("Error al preparar la consulta SQL: " . mysqli_error($conn));
+    }
+
+    // Asocia los parámetros con los valores
+    mysqli_stmt_bind_param($stmt, $nombre, $creador, $productora, $genero, $sistema_operativo, $fecha_lanzamiento);
+
+    // Ejecuta la consulta SQL
+    if (mysqli_stmt_execute($stmt)) {
+        //echo "Se ha añadido la información del juego";
+    } else {
+        //echo "Error al registrar el juego: " . mysqli_error($conn);
+    }
+
+    // Cierra la conexión y la declaración
+    mysqli_stmt_close($stmt);
+    mysqli_close($conn);
+
+}
+?>
---
+++ app/eliminar_videojuego.php
@@ -0,0 +1,4 @@
+<?php
+include("config.php"); // Incluye el archivo de configuración
+//desde página principal o desde buscar, seleccionar videojuego de catálogo --> pantalla videojuego con botón eliminar.
+?>
