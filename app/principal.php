1cd54efc97cfce594625d1ca257c31f8a1d77676
bidane <lnbidane@gmail.com>
2023-09-30 15:34:18 +0200

    catálogo chusco

--- app/principal.php
+++ app/principal.php
@@ -1,3 +1,23 @@
+<?php
+include("config.php"); // Incluye el archivo de configuración
+//desde página principal o desde buscar, seleccionar videojuego de catálogo --> pantalla videojuego con botón eliminar.
+if (!$conn) {
+    die("La conexión a la base de datos falló: " . mysqli_connect_error());
+}
+
+// Parámetros para la paginación
+$porPagina = 10; // Número de elementos por página
+$pagina = isset($_GET['pagina']) ? $_GET['pagina'] : 1;
+$inicio = ($pagina - 1) * $porPagina;
+
+// Consulta SQL con LIMIT para paginación
+$sql = "SELECT * FROM mytable LIMIT $inicio, $porPagina";
+$result = $conn->query($sql);
+
+
+
+?>
+
 <!DOCTYPE html>
 <html lang="es">
 <head>
@@ -5,6 +25,19 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="styles.css"> <!-- Agrega tu archivo CSS personalizado aquí -->
     <title>Goodgames</title>
+    <style>
+        table {
+            width: 100%;
+            border-collapse: collapse;
+        }
+        table, th, td {
+            border: 1px solid #ccc;
+        }
+        th, td {
+            padding: 10px;
+            text-align: left;
+        }
+    </style>
 </head>
 <body>
     <header>
@@ -14,6 +47,37 @@
     <main>
         <section>
             <h2>Catalogo de videojuegos</h2>
+            <table>
+        <tr>
+            <th>Nombre</th>
+            <th>Desarrollador</th>
+            <th>Género</th>
+        </tr>
+            <?php
+               while ($row = $result->fetch_assoc()) {
+                echo "<tr>";
+                echo "<td>" . $row["Name"] . "</td>";
+                echo "<td>" . $row["Developer"] . "</td>";
+                echo "<td>" . $row["Genre"] . "</td>";
+                echo "</tr>";
+            }
+            $sql = "SELECT COUNT(*) AS total FROM mytable";
+             $result = $conn->query($sql);
+             $fila = $result->fetch_assoc();
+            $totalPaginas = ceil($fila["total"] / $porPagina);
+
+            // Mostrar enlaces de paginación
+            echo "<div>";
+            for ($i = 1; $i <= $totalPaginas; $i++) {
+                if ($i == $pagina) {
+                    echo "<strong>$i</strong> ";
+                } else {
+                    echo "<a href='?pagina=$i'>$i</a> ";
+                }
+            }
+            echo "</div>";
+                ?>
+                </table>
             <!--  <form id="catalogo-videojuegos-form" action="buscar_videojuego.php" method="POST"> -->
             <label for="nombre">Nombre:</label>
                <!--  <input type="text" id="nombre" name="nombre" placeholder="Nombre" required><br>
