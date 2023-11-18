5ae0b8307b8d51ee2e3022218a830a0d1d5ec8fa
AnderG2003 <agorocica002@ikasle.ehu.eus>
2023-11-18 19:38:44 +0100

    Cambio en iniciar_web.sh

--- iniciar_web.sh
+++ iniciar_web.sh
@@ -3,8 +3,8 @@ echo "Vamos a cambiar los permisos de los logs"
 sudo chmod 644 logs.log
 sudo chown www-data logs.log
 echo "Vamos a cambiar los permisos de la clave simétrica"
-sudo chown www-data clave.txt
-sudo chmod 400 clave.txt 
+sudo chown www-data clave_simetrica
+sudo chmod 400 clave_simetrica.txt 
 cd ..
 echo "Ahora vamos a desplegar la web. Para abrirla hay que introducir en un navegador la siguiente url 'http://localhost:81/' "
 sudo docker build -t="web" .
