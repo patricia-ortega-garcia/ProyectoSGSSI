86ca3af1c855df947f26fd79581b0c5170e2b3d2
Patricia Ortega Garcia <patricia.ortega.garcia.02@gmail.com>
2023-11-23 22:19:51 +0100

    Cambiar parametros arreglado

--- app/ajustes_cuenta.php
+++ app/ajustes_cuenta.php
@@ -1,9 +1,13 @@
 <?php
 session_start();
+header("X-Frame-Options: SAMEORIGIN");
+header("X-Content-Type-Options: nosniff");
+header_remove("X-Powered-By");
+
 // Incluir archivo de configuración y verificar la sesión del usuario (debes implementar la lógica de autenticación)
 include("config.php");
 include("funciones.php");
-include("comprobar_contraseña.php")
+include("comprobar_contraseña.php");
 
 // Verificar la sesión del usuario (debes implementar esta lógica)
 if (!isset($_SESSION["usuario"])) {
@@ -19,14 +23,13 @@ $stmt = mysqli_prepare($conn, $sql);
 mysqli_stmt_bind_param($stmt, "s", $usuario);/*, $dni);*/
 mysqli_stmt_execute($stmt);
 $resultado = mysqli_stmt_get_result($stmt);
-//$datosUsuario = mysqli_fetch_assoc($resultado);
-$datosUsuario = $resultado->fetch_assoc();
+$datosUsuario = mysqli_fetch_assoc($resultado);
 /*Meter lo de descifrar*/
-$nm = descifrar($datosUsuario["nombre"]);
-$ap = descifrar($datosUsuario["apellidos"]);
-$tlf = descifrar($datosUsuario["telefono"]);
-$em = descifrar($datosUsuario["email"]);
-$fn = descifrar($datosUsuario["fecha_nacimiento"]);
+$nm = $datosUsuario["nombre"];
+$ap = $datosUsuario["apellidos"];
+$tlf = $datosUsuario["telefono"];
+$em = $datosUsuario["email"];
+$fn = $datosUsuario["fecha_nacimiento"];
 
 $sal = "";
 
@@ -43,6 +46,7 @@ if ($resultado) {
     echo "Error al recuperar la información del usuario: " . mysqli_error($conn);
 }
 
+
 mysqli_stmt_close($stmt);
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
@@ -54,26 +58,11 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $email = cifrar($_POST["email"]);
     /*$usuario = $_POST["username"];*/ //Hay que poner el DNI
     $password = $_POST["password"];
-    //$actual=$_SESSION["usuario"];
-
     if (comprobar($password)) {
         die("La contraseña es muy mala, cambiala");
     }
 
-    $nombresql="UPDATE usuarios_cod SET nombre=? WHERE username=? ";
-    $nombrestmt=mysqli_prepare($conn, $nombresql);
-
-    $apellidossql="UPDATE usuarios_cod SET apellidos=? WHERE username=? ";
-    $apellidosstmt=mysqli_prepare($conn, $apellidossql);
-
-    $telefonosql="UPDATE usuarios_cod SET telefono=? WHERE username=? ";
-    $telefonostmt=mysqli_prepare($conn, $telefonosql);
-
-    $fechasql="UPDATE usuarios_cod SET fecha_nacimiento=? WHERE username=? ";
-    $fechastmt=mysqli_prepare($conn, $fechasql);
-
-    $emailsql="UPDATE usuarios_cod SET email=? WHERE username=? ";
-    $emailstmt=mysqli_prepare($conn, $emailsql);
+    $actual=$_SESSION["usuario"];
 
     $nombresql="UPDATE usuarios_cod SET nombre=? WHERE username=? ";
     $nombrestmt=mysqli_prepare($conn, $nombresql);
@@ -98,29 +87,20 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     
     if(!empty($nombre)){
         mysqli_stmt_bind_param($nombrestmt, "ss", $nombre, $actual);
-        //$usuariosql="UPDATE usuarios_cod SET dni='$dni' WHERE dni='$actual' ";
-        $contrasenasql="UPDATE usuarios_cod SET contraseña=? WHERE username=? ";
-        $contrasenastmt=mysqli_prepare($conn, $contrasenasql);
-    }
-    
-    
-    if(!empty($nombre)){
-        mysqli_stmt_bind_param($nombrestmt, "ss", $nombre, $usuario);
         //$ejecutar2=mysqli_query($conn,$nombresql);
         if(mysqli_stmt_execute($nombrestmt)){
           ?> 
-          <h3 class="bien">¡Nombre modificado correctamente!</h3>
+          <!--<h3 class="bien">¡Nombre modificado correctamente!</h3>-->
             <?php
         }
     }
     
     if(!empty($apellidos)){
         mysqli_stmt_bind_param($apellidosstmt, "ss", $apellidos, $actual);
-        mysqli_stmt_bind_param($apellidosstmt, "ss", $apellidos, $usuario);
         //$ejecutar7=mysqli_query($conn,$apellidossql);
         if(mysqli_stmt_execute($apellidosstmt)){
           ?> 
-          <h3 class="bien">¡Apellido modificado correctamente!</h3>
+          <!--<h3 class="bien">¡Apellido modificado correctamente!</h3>-->
             <?php
         }
     }
@@ -128,11 +108,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if(!empty($telefono)){
         mysqli_stmt_bind_param($telefonostmt, "ss", $telefono, $actual);
         //$ejecutar3=mysqli_query($conn,$telefonosql);
-        mysqli_stmt_bind_param($telefonostmt, "ss", $telefono, $usuario);
-        //$ejecutar3=mysqli_query($conn,$telefonosql)
         if(mysqli_stmt_execute($telefonostmt)){
             ?> 
-            <h3 class="bien">¡Telefono modificado correctamente!</h3>
+            <!--<h3 class="bien">¡Telefono modificado correctamente!</h3>-->
             <?php
         }
     }
@@ -140,12 +118,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if(!empty($fecha_nacimiento)){
         mysqli_stmt_bind_param($fechastmt, "ss", $fecha_nacimiento, $actual);
         //$ejecutar4=mysqli_query($conn,$fechasql);
-
-        if($mysqli_stmt_execute($fechastmt)){
-        mysqli_stmt_bind_param($fechastmt, "ss", $fecha_nacimiento, $usuario);
-        //$ejecutar4=mysqli_query($conn,$fechasql);
+        if(mysqli_stmt_execute($fechastmt)){
             ?> 
-            <h3 class="bien">¡Fecha modificada correctamente!</h3>
+            <!--<h3 class="bien">¡Fecha modificada correctamente!</h3>-->
             <?php
         }
     }
@@ -153,11 +128,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if(!empty($email)){
         mysqli_stmt_bind_param($emailstmt, "ss", $email, $actual);
         //$ejecutar5=mysqli_query($conn,$emailsql);
-        if($mysqli_stmt_execute($emailstmt)){
-        mysqli_stmt_bind_param($emailstmt, "ss", $email, $usuario);
-        //$ejecutar5=mysqli_query($conn,$emailsql);
+        if(mysqli_stmt_execute($emailstmt)){
             ?> 
-                <h3 class="bien">¡Email modificado correctamente!</h3>
+                <!--<h3 class="bien">¡Email modificado correctamente!</h3> -->
                     <?php
         }
     }
@@ -167,10 +140,20 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         //$ejecutar6=mysqli_query($conn,$contrasenasql);
         if(mysqli_stmt_execute($contrasenastmt)){
             ?> 
-                <h3 class="bien">¡Contraseña modificada correctamente!</h3>
+                <!-- <h3 class="bien">¡Contraseña modificada correctamente!</h3> -->
                     <?php
         }
     }
+
+    /*
+    mysqli_stmt_close($nombrestmt);
+    mysqli_stmt_close($apellidosstmt);
+    mysqli_stmt_close($telefonostmt);
+    mysqli_stmt_close($fechastmt);
+    mysqli_stmt_close($emailstmt);
+    mysqli_stmt_close($contrasenastmt);
+    */
+
     if(!empty($nombre)){
         mysqli_stmt_close($nombrestmt);
     }
@@ -189,6 +172,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if(!empty($password)){
         mysqli_stmt_close($contrasenastmt);
     }
+
     mysqli_close($conn);
     
 }
@@ -213,19 +197,19 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
             <form id="ajustes-form" action="ajustes_cuenta.php" method="POST" onsubmit="return modificarUsuario();">
                 <label for="nombre">Nombre:</label>
-                <input type="text" id="nombre" name="nombre" placeholder="<?php echo $nm; ?>"><br>
+                <input type="text" id="nombre" name="nombre" placeholder="<?php echo descifrar($nm); ?>"><br>
 
                 <label for="apellidos">Apellidos:</label>
-                <input type="text" id="apellidos" name="apellidos" placeholder="<?php echo $ap; ?>"><br>
+                <input type="text" id="apellidos" name="apellidos" placeholder="<?php echo descifrar($ap); ?>"><br>
 
                 <label for="telefono">Teléfono:</label>
-                <input type="text" id="telefono" name="telefono" placeholder="<?php echo $tlf; ?>"><br>
+                <input type="text" id="telefono" name="telefono" placeholder="<?php echo descifrar($tlf); ?>"><br>
 
                 <label for="fecha_nacimiento">Fecha de Nacimiento:</label>
-                <input type="date" id="fecha_nacimiento" name="fecha_nacimiento" placeholder="<?php echo $fn; ?>"><br>
+                <input type="date" id="fecha_nacimiento" name="fecha_nacimiento" placeholder="<?php echo descifrar($fn); ?>"><br>
 
                 <label for="email">Email:</label>
-                <input type="email" id="email" name="email" placeholder="<?php echo $em; ?>"><br>
+                <input type="email" id="email" name="email" placeholder="<?php echo descifrar($em); ?>"><br>
 
                 <label for="username">Nombre de Usuario:</label>
                 <input type="text" id="username" name="username"><br>
