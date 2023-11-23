6f3b60e86b48ab08da61d1bf61819da8faca69f1
Patricia Ortega Garcia <patricia.ortega.garcia.02@gmail.com>
2023-11-23 17:43:43 +0100

    Update usuarios.sql

--- usuarios.sql
+++ usuarios.sql
@@ -9,6 +9,3 @@ CREATE TABLE usuarios (
     username VARCHAR(50) NOT NULL,
     password VARCHAR(255) NOT NULL
 );
-
-INSERT INTO `usuarios` (`nombre`, `apellidos`, `dni`, `telefono`, `fecha_nacimiento`, `email`, `username`, `password`) VALUES
-('Patricia', 'Ortega García', '79000919J', '615818650', '2002-1-07', 'patriciaorteaga@gmail.com', 'Pato', 'root1234');
