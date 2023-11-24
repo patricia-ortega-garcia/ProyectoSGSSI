1bbaa02652b58fe8c100163911d2076ab1a9b1db
Patricia Ortega Garcia <patricia.ortega.garcia.02@gmail.com>
2023-11-24 11:49:43 +0100

    Problema con la conexión con la base de datos solucionada. IMPORTANTE: Si dar fallos borrar carpeta mysql

--- app/config.php
+++ app/config.php
@@ -4,8 +4,8 @@ session_start();
 
 // Conexión a la base de datos
 $hostname = "db";
-$username = "admin";
-$password = "test";
+$username = "administradoresroot";
+$password = "ProyectoSGSSI#2023";
 $db = "database";
 
 $conn = mysqli_connect($hostname, $username, $password, $db);
