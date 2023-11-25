2fa067eafeeb4f8feab33b9659937cafc0abe9c7
Patricia Ortega Garcia <patricia.ortega.garcia.02@gmail.com>
2023-11-25 18:34:08 +0100

    Problemas de redireccion arreglados otra vez

--- app/modificar_videojuego.php
+++ app/modificar_videojuego.php
@@ -32,11 +32,12 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             // Verificar que la preparación fue exitosa
             if ($stmt) {
                 // Asociar los parámetros con los valores
-                mysqli_stmt_bind_param($stmt, "sssssi", $name, $dev, $prod, $gen, $op_sys, $date, $videojuego_id);
+                mysqli_stmt_bind_param($stmt, "ssssssi", $name, $dev, $prod, $gen, $op_sys, $date, $videojuego_id);
 
                 // Ejecutar la consulta preparada
                 if (mysqli_stmt_execute($stmt)) {
-                    echo "<script> window.location.replace('http://localhost:81/ver_videojuego.php'); </script> ";
+                    //echo "<script> window.location.replace('http://localhost:81/ver_videojuego.php'); </script> ";
+                    header("Location: ver_videojuego.php?id=" . $videojuego_id);
                     exit();
                 } else {
                     $error_message = "Error al guardar los cambios: " . mysqli_stmt_error($stmt);
