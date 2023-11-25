1b98032a4ddcdac74bb37ff25c70a8cb35d1c99b
AnderG2003 <agorocica002@ikasle.ehu.eus>
2023-11-25 15:03:38 +0100

    Cambio en el mensaje de la web

--- app/3fallosseguidos.php
+++ app/3fallosseguidos.php
@@ -9,11 +9,10 @@
 
     if ($_SESSION['incorrectosSeguidos'] == 3) {
         $_SESSION['incorrectosSeguidos'] = 0;
-    	echo "<h1> ¡Ups! </h1>";
-        echo "<p class = mensaje> Has fallado 5 veces seguidas, para asegurarte de que no eres un bot pulsa en el botón de abajo para volver </p>";
+        echo "<p class = mensaje> Tenemos sospechas de que eres un bot, pulsa en el botón de abajo que te llevará al inicio de sesión </p>";
     } else {
-    	echo "<h1> Vaya... Ha habido un error... </h1>";
-        echo "<p class = mensaje> Se ha intentado navegar desde una URl no permitida, pulse en el botón para volver al inicio de sesión </p>";
+    	echo "<h1> ERROR!! </h1>";
+        echo "<p class = mensaje> Se ha intentado navegar desde una URl no permitida, pulsa en el botón de abajo que te llevará al inicio de sesión </p>";
 }
 ?>
 <link rel="stylesheet" href="styles.css">
